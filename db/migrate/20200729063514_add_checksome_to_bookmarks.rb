5b0b8272499d9418cc21ff83faad594d66f9f8a6
須貝克仁 <>
2020-07-30 05:53:35 +0900

    no message

--- db/migrate/20200729063514_add_checksome_to_bookmarks.rb
+++
@@ -1,5 +0,0 @@
-class AddChecksomeToBookmarks < ActiveRecord::Migration[6.0]
-  def change
-    add_column :bookmarks, :checksome, :string
-  end
-end
--- db/migrate/20200729070916_add_facilities_to_bookmarks.rb
+++
@@ -1,5 +0,0 @@
-class AddFacilitiesToBookmarks < ActiveRecord::Migration[6.0]
-  def change
-    add_column :bookmarks, :facilities, :string
-  end
-end
